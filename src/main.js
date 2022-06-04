a5d59a69d7c273d8d10fcf5255b88a2275d5a228
عبدالله الحديدي <51133202+Abdallah2358@users.noreply.github.com>
2022-06-04 22:33:11 +0200

    removed unused import

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 
 import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
