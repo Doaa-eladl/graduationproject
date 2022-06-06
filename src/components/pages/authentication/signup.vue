c8519287a5fb57996ef02478a014411ced7f0c40
عبدالله الحديدي <51133202+Abdallah2358@users.noreply.github.com>
2022-06-06 15:06:26 +0200

    signup Done

--- src/components/pages/authentication/signup.vue
+++ src/components/pages/authentication/signup.vue
@@ -4,9 +4,15 @@
       <h2>انشاء حساب جديد</h2>
       <form @submit.prevent="signup">
         <div class="form-group">
-          <input type="text" class="form-control" placeholder="الاسم بالكامل" v-model="username" />
+          <input type="text" class="form-control" placeholder="الاسم بالكامل" v-model="name" />
           <small class="form-text text-muted" v-show="nameError">{{
               nameError
+          }}</small>
+        </div>
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="اسم المستخدم" v-model="userName" />
+          <small class="form-text text-muted" v-show="userNameError">{{
+              userNameError
           }}</small>
         </div>
         <div class="form-group">
@@ -35,6 +41,34 @@
               addressError
           }}</small>
         </div>
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="عمرك" v-model="age" />
+          <small class="form-text text-muted" v-show="ageError">{{
+              ageError
+          }}</small>
+        </div>
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="الرقم القومي" v-model="natID" />
+          <small class="form-text text-muted" v-show="natIDError">{{
+              natIDError
+          }}</small>
+        </div>
+        <div class="form-group">
+          <select v-model="bloodType" class="form-select">
+            <option disabled>اختر فصيلة دمك</option>
+            <option>O-</option>
+            <option>O+</option>
+            <option>A-</option>
+            <option>A+</option>
+            <option>B-</option>
+            <option>B+</option>
+            <option>AB-</option>
+            <option>AB+</option>
+          </select>
+          <small class="form-text text-muted" v-show="bloodTypeError">{{
+              bloodTypeError
+          }}</small>
+        </div>
         <div>
           <label>الموافقه علي الشروط والاحكام</label>
           <input type="checkbox" v-model="acceptTerms" />
@@ -58,67 +92,81 @@
 export default {
   data() {
     return {
-      username: null,
-      nameError: null,
+      name: null,
+      userName: null,
       email: null,
-      emailError: null,
       password: null,
-      passwordError: null,
-      phone: "",
-      phoneError: null,
+      phone: null,
       address: null,
-      addressError: null,
+      age: null,
+      natID: null,
+      bloodType: 'اختر فصيلة دمك',
       acceptTerms: false,
+
+      nameError: null,
+      userNameError: null,
+      emailError: null,
+      passwordError: null,
+      phoneError: null,
+      addressError: null,
+      ageError: null,
+      natIDError: null,
+      bloodTypeError: null,
       acceptTermsError: null,
 
     };
   },
   methods: {
-    signup() {
+    async signup() {
       if (this.isValidateInput()) {
         const userData = {
-          name: this.username,
+          name: this.name,
+          username: this.userName,
           email: this.email,
           phone: this.phone,
           password: this.password,
           address: this.address,
+          age: this.age,
+          nat_ID: this.natID,
+          blood: this.bloodType,
         }
         const api = this.api + '/users/create/';
-       
         this.axios.post(api, userData)
-          .then((response) => console.log(response))
+          .then((response) => {
+            //  TODO : add JWt
+            this.$router.replace("/")
+          })
           .catch((error) => console.log(error));
-        this.axios.post(api, userData)
-          .then((response) => console.log(response))
-          .catch((error) => console.log(error));
-        this.$router.replace("/")
-      }
-      //هنبعت للباك الداتا لو كله سليم ع هيئه اوبجيكت
-      else {
-
       }
     },
     isValidateInput() {
       this.restErrors()
       let isValid = true;
 
-      if (!this.username) {
+      if (!this.name) {
         this.nameError = "ادخل الاسم بالكامل";
         isValid = false
       }
-
+      if (!this.userName) {
+        this.userNameError = "ادخل اسم المستخدم";
+        isValid = false
+      }
       if (!this.email) {
         this.emailError = "ادخل بريدك الالكتروني";
         isValid = false
       }
+      const pattern = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
 
       if (!this.password) {
         this.passwordError = "ادخل كلمه المرور";
         isValid = false
+      } else if (!pattern.test(this.password)) {
+        this.passwordError = "ادخل كلمه المرور مكونه من ثماني حروف و تحتوي رقم و حرف علي الاقل";
+        isValid = false
       }
 
       if (this.phone) {
-        if (this.phone.length < 12) {
+        if (this.phone.length != 12) {
           this.phoneError = "ادخل رقمك المكون من 12 رقم";
           isValid = false
         } else if (isNaN(this.phone)) {
@@ -134,7 +182,14 @@ export default {
         this.addressError = "ادخل عنوانك";
         isValid = false
       }
-
+      if (!this.age) {
+        this.ageError = "ادخل عمرك";
+        isValid = false
+      }
+      if (!this.natID) {
+        this.natIDError = "ادخل الرقم القومي";
+        isValid = false
+      }
       if (!this.acceptTerms) {
         this.acceptTermsError = "يجب الموافقه علي شروط الموقع";
         isValid = false
@@ -145,10 +200,14 @@ export default {
     },
     restErrors() {
       this.nameError = null;
-      this.emailError = null;
+      this.userNameError = null,
+        this.emailError = null;
       this.passwordError = null;
       this.phoneError = null;
       this.addressError = null;
+      this.ageError = null;
+      this.natIDError = null;
+      this.bloodTypeError = null;
       this.acceptTermsError = null;
     }
   },
