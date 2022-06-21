14fc97c5dcaf0560ad0e911aefe841f28ea7f172
عبدالله الحديدي <51133202+Abdallah2358@users.noreply.github.com>
2022-06-21 22:55:00 +0200

    refactored axois and main js

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 //axios.defaults.baseURL = 'http://localhost:8000/api';
-axios.defaults.baseURL = 'http://localhost:3258';
+axios.defaults.baseURL = 'http://localhost:8000/api';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 //axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 
--- src/main.js
+++ src/main.js
@@ -3,21 +3,20 @@ import { createApp } from 'vue'
 import App from '@/App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap';
+import router from '@/router';
 import VueAxios from 'vue-axios'
 import axios from './axios'
+import { createPinia } from "pinia";
 
 import basebutton from '@/components/UI/basebutton.vue'
 import basewrapper from '@/components/UI/basewrapper.vue'
 
-import router from '@/router';
 
 const app = createApp(App)
 
-
+app.config.globalProperties.auth = false;
 import { dom } from '@fortawesome/fontawesome-svg-core'
 dom.watch()
-import { createPinia } from 'pinia'
-app.use(createPinia())
 
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
@@ -28,6 +27,7 @@ library.add(faPhone,faEnvelope,faLocationDot,faSearch)
 app.component('fa', FontAwesomeIcon)
 app.use(router)
 app.use(VueAxios, axios)
+app.use(createPinia())
 app.component('base-button',basebutton)
 app.component('base-wrapper',basewrapper)
 
